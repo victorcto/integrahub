0a35265c004ac4b97c6062fbeb0a3a7a1c7abc6e
VIctor Couto <victorcto@outlook.com>
2025-04-11 23:58:43 -0300

    initial commit

---
+++ src/main/java/br/com/integrahub/IntegrahubApplication.java
@@ -0,0 +1,13 @@
+package br.com.integrahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrahubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrahubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/integrahub/IntegrahubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.integrahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrahubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
